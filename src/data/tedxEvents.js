3164588141e16d40cb082c62c55c4ee6bfe147d1
Henry Wu <henrywu0730@hotmail.com>
2026-07-16 16:26:56 +0800

    Add TEDx event data

---
+++ src/data/tedxEvents.js
@@ -0,0 +1,58 @@
+export const tedxEvents = [
+  {
+    year: "2026",
+    title: "TEDx Youth@JLHS 2026",
+    theme: "",
+    description: "",
+    talks: [
+      {
+        speaker: "",
+        title: "",
+        youtube: "",
+        bilibili: "",
+      },
+    ],
+  },
+  {
+    year: "2025",
+    title: "TEDx Youth@JLHS 2025",
+    theme: "",
+    description: "",
+    talks: [
+      {
+        speaker: "",
+        title: "",
+        youtube: "",
+        bilibili: "",
+      },
+    ],
+  },
+  {
+    year: "2024",
+    title: "TEDx Youth@JLHS 2024",
+    theme: "",
+    description: "",
+    talks: [
+      {
+        speaker: "",
+        title: "",
+        youtube: "",
+        bilibili: "",
+      },
+    ],
+  },
+  {
+    year: "2023",
+    title: "TEDx Youth@JLHS 2023",
+    theme: "",
+    description: "",
+    talks: [
+      {
+        speaker: "",
+        title: "",
+        youtube: "",
+        bilibili: "",
+      },
+    ],
+  },
+];
